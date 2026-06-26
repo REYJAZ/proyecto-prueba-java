f5e20c6e953f562ac180518709f38568b3c1dd6a
Jamesostin-15-SY <jamesostin15@gmail.com>
2026-06-25 20:30:13 -0500

    HOLA

--- src/Modelo/Embarque.java
+++ src/Modelo/Embarque.java
@@ -3,7 +3,7 @@ package Modelo;
 import javax.swing.JOptionPane;// MANEJADOR DE VENTANAS
 
 public class Embarque {
-    // UNO, DOS, TRES.....
+    // UNO, DOS, TRES.... hello
     private String codigo;
     private String distribuidor;
     private String tipoladrillo;
